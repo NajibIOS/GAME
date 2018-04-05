06838139977e2e437928fd461ee33bfe904205f1
Najib <contact.annabi@gmail.com>
2018-04-05 10:32:55 +0200

    Create a new weapon "Crossbow"

---
+++ Weapon/Crossbow.swift
@@ -0,0 +1,17 @@
+//
+//  Crossbow.swift
+//  GAME
+//
+//  Created by abdel on 05/04/2018.
+//  Copyright © 2018 Najib ANNABi. All rights reserved.
+//
+
+import Foundation
+
+class Crossbow : Weapon {
+    
+    // FEATURES - Init
+    init() {
+        super.init(name: "crossbow", damage: 20, magicPower: 0)
+    }
+}
